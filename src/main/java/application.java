f607c5fff9a40964412a5472217658b2de5a4054
wangqifan <1694675518@qq.com>
2018-11-19 23:57:40 +0800

    --

--- src/main/java/application.java
+++ src/main/java/application.java
@@ -8,12 +8,16 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class application {
     public static void  main(String[] args)throws Exception {
 
 
         BrowserMobProxy proxy = new BrowserMobProxyServer();
         proxy.start(0);
+        proxy.addHeader("User-Agent","Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
         Proxy seleniumProxy = ClientUtil.createSeleniumProxy(proxy);
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
